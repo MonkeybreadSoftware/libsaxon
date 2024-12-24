7e41c4ef09ef36079e9dae28e5be977b9544c643
Christian Schmitz <support@monkeybreadsoftware.de>
2024-12-24 13:22:45 +0100

    Addes test project

---
+++ SaxonTest/SaxonTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SaxonTest
+//
+//  Created by Christian Schmitz on 24.12.24.
+//
+
+#include <iostream>
+#include "SaxonProcessor.h"
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
